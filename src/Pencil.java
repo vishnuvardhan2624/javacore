ccf29c6c7ed232301f5f637696cf43ec7677044c
vis40 <vishnuchevalla@gmail.com>
2026-09-11 12:29:23 +0530

    program added

---
+++ src/Pencil.java
@@ -0,0 +1,17 @@
+
+public class Pencil {
+
+	public static void main(String[] args) {
+      int money=100;
+      int pencil_cost=7;
+      
+      int noof_penclis_buyed= money/pencil_cost;
+      
+      int money_left = money%pencil_cost;
+      
+      System.out.println("number of pencils buyed ="+noof_penclis_buyed);
+      System.out.println("money remaining="+money_left );
+      
+	}
+
+}
